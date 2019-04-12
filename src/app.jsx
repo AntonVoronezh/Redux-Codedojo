d1d1f19ea2268eb8d9c9709285d4d346bbf3079e
AntonVoronezh <pstut@mail.ru>
2019-04-12 20:15:46 +0300

    11#1

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Store from './store';
+
+const initialState = { count: 0 };
+
+function updateState(state, action) {
+    switch(action.type) {
+        
+    }
+};
 
 class Counter extends React.Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = { count: 0 };
+		this.increment = this.increment.bind(this);
+		this.decrement = this.decrement.bind(this);
+	}
 
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-    }
+	increment() {
+	}
 
-    increment() {
-        this.setState({ count: this.state.count + 1 });
-    }
+	decrement() {
+	}
 
-    decrement() {
-        this.setState({ count: this.state.count - 1 });
-    }
+	render() {
+		return (
+			<div className="counter">
+				<span className="count">{this.state.count}</span>
 
-    render() {
-        return (
-            <div className="counter">
-                <span className="count">{this.state.count}</span>
-
-                <div className="buttons">
-                    <button className="decrement" onClick={this.decrement}>-</button>
-                    <button className="increment" onClick={this.increment}>+</button>
-                </div>
-            </div>
-        );
-    }
+				<div className="buttons">
+					<button className="decrement" onClick={this.decrement}>
+						-
+					</button>
+					<button className="increment" onClick={this.increment}>
+						+
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
